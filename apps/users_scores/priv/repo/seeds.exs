505bb8550007f9c092dcdc4c7978cb7d9418d99f
MichalWojcieszyk <mwojcieszyk@gmail.com>
2023-02-12 18:34:26 +0100

    add 1_000_000 users creation in seed file

--- apps/users_scores/priv/repo/seeds.exs
+++ apps/users_scores/priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias UsersScores.User
+
+date_time_utc_now = DateTime.truncate(DateTime.utc_now(), :second)
+params = %{inserted_at: date_time_utc_now, updated_at: date_time_utc_now}
+
+IO.puts("There will be 1000000 users created")
+
+User.Mutator.create_users(1_000_000, params)
+
+IO.puts("There are 1000000 users created in database with 0 as a points score")
